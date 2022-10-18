ef9c8f5a825f1f2ada526c297d3070f0d13cfc84
Shahaf Ohayon Moran <shahafmoran@gmail.com>
2022-10-18 23:56:39 +0300

    more https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   weatherApi: {
     key: "6AClZTYm6UOfmeUwVzUHrX9mLoqkjfRg",
-    currentWeather: "http://dataservice.accuweather.com/currentconditions/v1/",
-    forecast: "http://dataservice.accuweather.com/forecasts/v1/daily/5day/",
-    autocomplete: "http://dataservice.accuweather.com/locations/v1/cities/autocomplete"
+    currentWeather: "https://dataservice.accuweather.com/currentconditions/v1/",
+    forecast: "https://dataservice.accuweather.com/forecasts/v1/daily/5day/",
+    autocomplete: "https://dataservice.accuweather.com/locations/v1/cities/autocomplete"
   }
 };
